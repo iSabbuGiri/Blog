d36fb256f455d52092eddccb715ad685c32f47ac
Sabbu Giri <sabbugiri50@gmail.com>
2021-05-03 12:35:18 +0545

    added categories

--- theblog/views.py
+++ theblog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
+from .models import Post, Category
 from .forms import PostForm, EditForm
 from django.urls import reverse_lazy
 
@@ -26,6 +26,15 @@ class AddPostView(CreateView):
    # fields = '__all__'
     #fields = ('title', 'body')
 
+
+    ordering = ['published_date']
+
+class AddCategoryView(CreateView):
+    model = Category
+    #form_class = PostForm
+    template_name = 'add_category.html'
+    fields = '__all__'
+    #fields = ('title', 'body')
 
 class UpdatePostView(UpdateView):
     model = Post
