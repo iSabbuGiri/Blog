d506e1a72fe7372850641518fd2d886f5b52ca08
Sabbu Giri <sabbugiri50@gmail.com>
2021-05-06 20:02:35 +0545

    added profile pic and social media field

--- theblog/models.py
+++ theblog/models.py
@@ -15,13 +15,17 @@ class Category(models.Model):
        # return reverse('article-detail',args=(str(self.id)))
        return reverse('home')
 
-
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     bio = models.TextField()
+    profile_pic = models.ImageField(null=True, blank=True, upload_to='images/profile/')
+    facebook_url = models.CharField(max_length=255, null=True, blank=True)
+    instagram_url = models.CharField(max_length=255, null=True, blank=True)
+    twitter_url = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return str(self.user)
+
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
