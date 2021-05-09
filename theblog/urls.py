386773536be1dce91d169c24dbe4faa81fdbeab2
Sabbu Giri <sabbugiri50@gmail.com>
2021-05-09 21:17:30 +0545

    post comments

--- theblog/urls.py
+++ theblog/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 #from . import views
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, CategoryListView,LikeView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, CategoryListView,LikeView,AddCommentView
 urlpatterns = [
     #path('', views.home, name="home"),
     path('', HomeView.as_view(), name="home"),
@@ -13,5 +13,6 @@ urlpatterns = [
     path('category/<str:cats>/', CategoryView, name='category'),
     path('category_list/', CategoryListView, name='category-list'),
     path('like/<int:pk>', LikeView, name='like_post'),
+    path('article/<int:pk>/comment/', AddCommentView.as_view(), name="add_comment"),
     
 ]
