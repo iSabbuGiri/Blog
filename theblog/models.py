f29c02a3e4b2905487c8ace6c41e2e9c5d6dff35
Sabbu Giri <sabbugiri50@gmail.com>
2021-05-09 13:36:40 +0545

    default profile picture

--- theblog/models.py
+++ theblog/models.py
@@ -18,13 +18,19 @@ class Category(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     bio = models.TextField()
-    profile_pic = models.ImageField(null=True, blank=True, upload_to='images/profile/')
+    profile_picture = models.ImageField(null=True, blank=True, upload_to='images/profile/')
     facebook_url = models.CharField(max_length=255, null=True, blank=True)
     instagram_url = models.CharField(max_length=255, null=True, blank=True)
     twitter_url = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return str(self.user)
+
+    def get_profile_picture(self):
+        if self.profile_picture:
+            return profile_picture_url
+        else:
+            return 'your_default_img_url_path'    
 
 
 class Post(models.Model):
